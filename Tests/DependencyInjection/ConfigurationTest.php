9e0a4de17564a403774923b9be6e0aa6ca59b419
Richard Tuin <richardtuin@gmail.com>
2015-04-09 07:44:19 +0200

    Validate if the default bus exists

--- Tests/DependencyInjection/ConfigurationTest.php
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -61,18 +61,66 @@ class ConfigurationTest extends AbstractConfigurationTestCase
 
     public function testDefaultMiddlewareMustExist()
     {
-        $this->assertConfigurationIsValid([
-            'tactician' => [
-                'default_bus' => 'foo',
-                'commandbus' => [
-                    'bar' => [
-                        'middleware' => [
-
+        $this->assertConfigurationIsInvalid(
+            [
+                'tactician' => [
+                    'default_bus' => 'foo',
+                    'commandbus' => [
+                        'bar' => [
+                            'middleware' => [
+                                'my_middleware'  => 'some_middleware',
+                            ]
                         ]
                     ]
                 ]
-            ]
-        ]);
-        $this->markTestIncomplete('Not sure how to implement this with configuration invalidation.');
+            ],
+            'The default_bus "foo" was not defined as command bus.'
+        );
+
+        $this->assertConfigurationIsInvalid(
+            [
+                'tactician' => [
+                    'commandbus' => [
+                        'bar' => [
+                            'middleware' => [
+                                'my_middleware'  => 'some_middleware',
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            'The default_bus "default" was not defined as command bus.'
+        );
+    }
+
+    public function testMiddlewareDefinitionCannotBeEmpty()
+    {
+        $this->assertConfigurationIsInvalid(
+            [
+                'tactician' => [
+                    'commandbus' => [
+                        'default' => [
+                            'middleware' => [
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            'The path "tactician.commandbus.default.middleware" should have at least 1 element(s) defined.'
+        );
+
+        $this->assertConfigurationIsInvalid(
+            [
+                'tactician' => [
+                    'commandbus' => [
+                        'foo' => [
+                            'middleware' => [
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            'The path "tactician.commandbus.foo.middleware" should have at least 1 element(s) defined.'
+        );
     }
 }
