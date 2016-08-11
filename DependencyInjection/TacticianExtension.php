48e177452465eee98b07e056c454e832b1fa8c71
Ron Rademaker <ron@connectholland.nl>
2016-08-11 16:46:01 +0200

    Only load the voter if security middleware is activated

--- DependencyInjection/TacticianExtension.php
+++ DependencyInjection/TacticianExtension.php
@@ -75,12 +75,27 @@ class TacticianExtension extends ConfigurableExtension
     }
 
     /**
-     * Configure the security voter.
+     * Configure the security voter if the security middleware is loaded.
      *
      * @param array $mergedConfig
      * @param ContainerBuilder $container
      */
     private function configureSecurity(array $mergedConfig, ContainerBuilder $container)
+    {
+        foreach ($mergedConfig['commandbus'] as $commandBusConfig) {
+            if (in_array('tactician.middleware.security', $commandBusConfig['middleware'])) {
+                $this->configureCommandSecurityVoter($mergedConfig, $container);
+            }
+        }
+    }
+
+    /**
+     * Configure the security voter.
+     *
+     * @param array $mergedConfig
+     * @param ContainerBuilder $container
+     */
+    private function configureCommandSecurityVoter(array $mergedConfig, ContainerBuilder $container)
     {
         if (!$container->has('tactician.middleware.security_voter')) {
             $definition = new Definition(
