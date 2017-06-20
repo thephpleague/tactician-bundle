acbd8d57346ecb854c32644de81cc347b3ead730
Ron Rademaker <ron@connectholland.nl>
2017-06-20 09:02:00 +0200

    Use testcase labels as keys

--- tests/Security/Voter/HandleCommandVoterTest.php
+++ tests/Security/Voter/HandleCommandVoterTest.php
@@ -47,38 +47,17 @@ class HandleCommandVoterTest extends TestCase
     public function provideTestVoteData()
     {
         return [
-            // Testcase: default access is false
-            ['handle', new FakeCommand, [new Role('ROLE_ADMIN')], [], VoterInterface::ACCESS_DENIED],
-
-            // Testcase: abstain when not handling a command, but using the handle attribute
-            ['handle', null, [new Role('ROLE_ADMIN')], [], VoterInterface::ACCESS_ABSTAIN],
-
-            // Testcase: abstain when not handling a command and not using the handle attribute
-            ['create', null, [new Role('ROLE_ADMIN')], [], VoterInterface::ACCESS_ABSTAIN],
-
-            // Testcase: abstain when not handling a command
-            ['create', new FakeCommand, [new Role('ROLE_ADMIN')], [FakeCommand::class => ['ROLE_ADMIN']], VoterInterface::ACCESS_ABSTAIN],
-
-            // Testcase: default is unrelated to roles
-            ['handle', new FakeCommand, [new Role('ROLE_ADMIN')], [], VoterInterface::ACCESS_DENIED],
-
-            // Testcase: deny access if incorrect role
-            ['handle', new FakeCommand, [new Role('ROLE_ADMIN')], [FakeCommand::class => ['ROLE_USER']], VoterInterface::ACCESS_DENIED],
-
-            // Testcase: grant access if the user has the configure role
-            ['handle', new FakeCommand, [new Role('ROLE_USER')], [FakeCommand::class => ['ROLE_USER']], VoterInterface::ACCESS_GRANTED],
-
-            // Testcase: grant access if the user has an inherited role
-            ['handle', new FakeCommand, [new Role('ROLE_ROOT')], [FakeCommand::class => ['ROLE_USER']], VoterInterface::ACCESS_GRANTED],
-
-            // Testcase: grant access if the user has one of the configure roles
-            ['handle', new FakeCommand, [new Role('ROLE_USER')], [FakeCommand::class => ['ROLE_USER', 'ROLE_TWO']], VoterInterface::ACCESS_GRANTED],
-
-            // Testcase: grant access if the user has one of the configure roles, but also another role
-            ['handle', new FakeCommand, [new Role('ROLE_USER', new Role('ROLE_THREE'))], [FakeCommand::class => ['ROLE_USER', 'ROLE_TWO']], VoterInterface::ACCESS_GRANTED],
-
-            // Testcase: deny access if the command is not in the mapping (i.e. a default deny access case)
-            ['handle', new FakeCommand, [new Role('ROLE_USER')], ['someOtherCommand' => ['ROLE_USER']], VoterInterface::ACCESS_DENIED],
+            'default access is false' => ['handle', new FakeCommand, [new Role('ROLE_ADMIN')], [], VoterInterface::ACCESS_DENIED],
+            'abstain when not handling a command, but using the handle attribute' => ['handle', null, [new Role('ROLE_ADMIN')], [], VoterInterface::ACCESS_ABSTAIN],
+            'abstain when not handling a command and not using the handle attribute' => ['create', null, [new Role('ROLE_ADMIN')], [], VoterInterface::ACCESS_ABSTAIN],
+            'abstain when not handling a command' => ['create', new FakeCommand, [new Role('ROLE_ADMIN')], [FakeCommand::class => ['ROLE_ADMIN']], VoterInterface::ACCESS_ABSTAIN],
+            'default is unrelated to roles' => ['handle', new FakeCommand, [new Role('ROLE_ADMIN')], [], VoterInterface::ACCESS_DENIED],
+            'deny access if incorrect role' => ['handle', new FakeCommand, [new Role('ROLE_ADMIN')], [FakeCommand::class => ['ROLE_USER']], VoterInterface::ACCESS_DENIED],
+            'grant access if the user has the configure role' => ['handle', new FakeCommand, [new Role('ROLE_USER')], [FakeCommand::class => ['ROLE_USER']], VoterInterface::ACCESS_GRANTED],
+            'grant access if the user has an inherited role' => ['handle', new FakeCommand, [new Role('ROLE_ROOT')], [FakeCommand::class => ['ROLE_USER']], VoterInterface::ACCESS_GRANTED],
+            'grant access if the user has one of the configure roles' => ['handle', new FakeCommand, [new Role('ROLE_USER')], [FakeCommand::class => ['ROLE_USER', 'ROLE_TWO']], VoterInterface::ACCESS_GRANTED],
+            'grant access if the user has one of the configure roles, but also another role' => ['handle', new FakeCommand, [new Role('ROLE_USER', new Role('ROLE_THREE'))], [FakeCommand::class => ['ROLE_USER', 'ROLE_TWO']], VoterInterface::ACCESS_GRANTED],
+            'deny access if the command is not in the mapping (i.e. a default deny access case)' => ['handle', new FakeCommand, [new Role('ROLE_USER')], ['someOtherCommand' => ['ROLE_USER']], VoterInterface::ACCESS_DENIED],
         ];
     }
 }
