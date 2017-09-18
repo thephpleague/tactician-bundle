9cd8121e937164c154bbce8f34dfc824a2ef25bf
Ross Tuck <me@rosstuck.com>
2017-09-18 22:35:54 +0200

    Remove now-unused UnknownMiddlewareException

--- tests/Integration/ValidatorTest.php
+++ tests/Integration/ValidatorTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace League\Tactician\Bundle\Tests\Integration;
 
-use League\Tactician\Bundle\DependencyInjection\Compiler\UnknownMiddlewareException;
 use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 
 /**
