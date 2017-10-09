7f4560a13c1123c566b587c538de25ef4257ada1
Ross Tuck <me@rosstuck.com>
2017-10-09 12:20:32 +0200

    Add tests for HandlerMapping in CommandHandlerPass

--- tests/DependencyInjection/Compiler/CommandHandlerPassTest.php
+++ tests/DependencyInjection/Compiler/CommandHandlerPassTest.php
@@ -5,7 +5,11 @@ namespace League\Tactician\Bundle\Tests\DependencyInjection\Compiler;
 use League\Tactician\Bundle\DependencyInjection\Compiler\CommandHandlerPass;
 use League\Tactician\Bundle\DependencyInjection\HandlerMapping\ClassNameMapping;
 use League\Tactician\Bundle\DependencyInjection\HandlerMapping\HandlerMapping;
+use League\Tactician\Bundle\DependencyInjection\HandlerMapping\Routing;
+use League\Tactician\Bundle\Tests\Fake\FakeCommand;
+use League\Tactician\Bundle\Tests\Fake\OtherFakeCommand;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class CommandHandlerPassTest extends TestCase
@@ -19,8 +23,6 @@ class CommandHandlerPassTest extends TestCase
     {
         $this->mappingStrategy = new ClassNameMapping();
     }
-
-    // TODO: Somehow assert that routing is run and mapped onto the locators
 
     public function testAddingSingleDefaultBus()
     {
@@ -61,6 +63,60 @@ class CommandHandlerPassTest extends TestCase
         $this->assertTrue($container->hasDefinition('tactician.commandbus.other_bus'));
 
         $this->assertDefaultAliasesAreDeclared($container, 'custom_bus');
+    }
+
+    public function test_handler_mapping_is_called()
+    {
+        $container = $this->containerWithConfig(
+            [
+                'commandbus' => [ 'default' => ['middleware' => []] ]
+            ]
+        );
+
+        $routing = new Routing(['default']);
+        $routing->routeToAllBuses(FakeCommand::class, 'some.handler');
+
+        $mapping = $this->prophesize(HandlerMapping::class);
+        $mapping->build($container, Argument::type(Routing::class))->willReturn($routing);
+
+        (new CommandHandlerPass($mapping->reveal()))->process($container);
+
+        $this->assertEquals(
+            [FakeCommand::class => 'some.handler'],
+            $container->getDefinition('tactician.commandbus.default.handler.locator')->getArgument(1)
+        );
+    }
+
+    public function test_handler_mapping_is_kept_bus_specific()
+    {
+        $container = $this->containerWithConfig(
+            [
+                'default_bus' => 'bus.a',
+                'commandbus' => [
+                    'bus.a' => ['middleware' => []],
+                    'bus.b' => ['middleware' => []]
+                ]
+            ]
+        );
+
+        $routing = new Routing(['bus.a', 'bus.b']);
+        $routing->routeToBus('bus.a', FakeCommand::class, 'some.handler.a');
+        $routing->routeToBus('bus.b', FakeCommand::class, 'some.handler.b');
+        $routing->routeToAllBuses(OtherFakeCommand::class, 'some.other.handler');
+
+        $mapping = $this->prophesize(HandlerMapping::class);
+        $mapping->build($container, Argument::type(Routing::class))->willReturn($routing);
+
+        (new CommandHandlerPass($mapping->reveal()))->process($container);
+
+        $this->assertEquals(
+            [FakeCommand::class => 'some.handler.a', OtherFakeCommand::class => 'some.other.handler'],
+            $container->getDefinition('tactician.commandbus.bus.a.handler.locator')->getArgument(1)
+        );
+        $this->assertEquals(
+            [FakeCommand::class => 'some.handler.b', OtherFakeCommand::class => 'some.other.handler'],
+            $container->getDefinition('tactician.commandbus.bus.b.handler.locator')->getArgument(1)
+        );
     }
 
     private function containerWithConfig($config)
