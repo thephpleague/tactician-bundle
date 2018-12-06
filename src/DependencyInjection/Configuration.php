8d981903bd772497cfe7bef8cd3382166c89e391
jildertmiedema <git@jildertmiedema.nl>
2018-12-06 16:06:11 +0100

    Remove Symfony 4.2 deprecation

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -21,8 +21,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('tactician');
+        $treeBuilder = new TreeBuilder('tactician');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('tactician');
+        }
 
         $rootNode
             ->children()
